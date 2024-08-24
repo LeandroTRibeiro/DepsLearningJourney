69bbec47f5b216afe794c4692ee0049f4ab1ce8b
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-08-24 16:38:34 -0300

    feat: add Variables examples

--- src/c#/Variables/Variables/Program.cs
+++ src/c#/Variables/Variables/Program.cs
@@ -4,35 +4,42 @@ namespace Variables
 {
     class Program
     {
-        // Variável global (de instância) - acessível em toda a classe, mas precisa de uma instância
+        // Variável de instância - acessível em toda a classe, mas precisa de uma instância
         string globalInstanceVariable = "Global Instance Variable";
         
-        // Variável global (estática) - acessível em toda a classe, sem precisar de uma instância
+        // Variável estática - acessível em toda a classe, sem precisar de uma instância
         static string globalStaticVariable = "Global Static Variable";
+        
+        // Variável privada (de instância) - acessível em toda a classe, mas precisa de uma instância
+        private string privateVariable = "Private Variable";
         
         static void Main(string[] args)
         {
-
-            // Instanciando a classe para acessar a variavel
+            // Instanciando a classe para acessar as variáveis de instância
             Program program = new Program();
-
-            // Variavel Local
+            
+            // Acessando a variável de instância
+            Console.WriteLine($"Valor da variável de instância: {program.globalInstanceVariable}");
+            
+            // Acessando a variável estática
+            Console.WriteLine($"Valor da variável estática: {globalStaticVariable}");
+            
+            // Acessando a variável privada de instância
+            Console.WriteLine($"Valor da variável privada de instância: {program.privateVariable}");
+            
+            // Variável Local
             string localVariable = "Local Variable";
             
-            // Acessando a variável global de instância
-            Console.WriteLine($"Valor da variável global de instância: {program.globalInstanceVariable}");
-            
-            // Acessando a variável global estática
-            Console.WriteLine($"Valor da variável global estática: {globalStaticVariable}");
-            
             // Acessando a variável local
-            Console.WriteLine($"Valor da variáve local: {localVariable}");
+            Console.WriteLine($"Valor da variável local: {localVariable}");
             
-            // Declaracao de uma constante
+            // Declaração de uma constante
             const string constantVariable = "Constant Variable";
             Console.WriteLine($"Valor da constante: {constantVariable}");
             
+            // Declaração de uma variável com inferência de tipo
+            var inferredVariable = "Inferred Variable";
+            Console.WriteLine($"Valor da variável com inferência de tipo: {inferredVariable}");
         }
     }
 }
-
