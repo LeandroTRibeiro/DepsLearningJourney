3c4135c3448e11ff1729d647a4e79d850bd82bc1
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-08-24 16:34:04 -0300

    feat: add c# logo image

---
+++ src/c#/Variables/Variables/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        // Variável global (de instância) - acessível em toda a classe, mas precisa de uma instância
+        string globalInstanceVariable = "Global Instance Variable";
+        
+        // Variável global (estática) - acessível em toda a classe, sem precisar de uma instância
+        static string globalStaticVariable = "Global Static Variable";
+        
+        static void Main(string[] args)
+        {
+
+            // Instanciando a classe para acessar a variavel
+            Program program = new Program();
+
+            // Variavel Local
+            string localVariable = "Local Variable";
+            
+            // Acessando a variável global de instância
+            Console.WriteLine($"Valor da variável global de instância: {program.globalInstanceVariable}");
+            
+            // Acessando a variável global estática
+            Console.WriteLine($"Valor da variável global estática: {globalStaticVariable}");
+            
+            // Acessando a variável local
+            Console.WriteLine($"Valor da variáve local: {localVariable}");
+            
+            // Declaracao de uma constante
+            const string constantVariable = "Constant Variable";
+            Console.WriteLine($"Valor da constante: {constantVariable}");
+            
+        }
+    }
+}
+
