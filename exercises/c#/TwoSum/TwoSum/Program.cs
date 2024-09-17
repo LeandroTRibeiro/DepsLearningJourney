847ea10587ae4492d9a6b480b5f727299994a62f
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-09-17 16:18:01 -0300

    feat: add TwoSum exercise

---
+++ exercises/c#/TwoSum/TwoSum/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TwoSum;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = new() { 3, 2, 4 };
+        int target = 6;
+        
+        TwoSum(numbers, target);
+
+    }
+    static void TwoSum(List<int> numbers, int target)
+    {
+        for (int i = 0; i < numbers.Count; i++)
+        {
+            for (int j = i + 1; j < numbers.Count; j++)
+            {
+                if (numbers[i] + numbers[j] == target)
+                {
+                    Console.WriteLine($"A soma de {numbers[i]} + {numbers[j]} = {target}, que sao os indices {i} e {j}");
+                    return;
+                }
+            }
+        }
+        
+        Console.WriteLine($"Nenhuma soma dos numeros do array dao o resultado de {target}");
+    }
+}
