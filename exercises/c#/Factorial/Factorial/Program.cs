b2550e34305f4d7d5b7c72933b89958cc9246bb0
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-09-17 16:15:59 -0300

    feat: add Factorial exercise

---
+++ exercises/c#/Factorial/Factorial/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Numerics;
+
+namespace Factorial;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Factorial(5));
+    }
+
+    static BigInteger Factorial(int n)
+    {
+        return n == 0 ? 1 : n * Factorial(n - 1);
+    }
+}
