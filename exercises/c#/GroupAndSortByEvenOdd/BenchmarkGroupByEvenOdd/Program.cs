88fc395d74d938ad9d60f6db7463834a77ab8f5e
Leandro Thiago Ribeiro <leandrothiago_ribeiro@hotmail.com>
2024-10-24 13:23:51 -0300

    feat: add AndSortByEvenOdd

---
+++ exercises/c#/GroupAndSortByEvenOdd/BenchmarkGroupByEvenOdd/Program.cs
@@ -0,0 +1,35 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace BenchmarkGroupByEvenOdd;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<BenchmarkTest>();
+    }
+
+    public class BenchmarkTest
+    {
+        private readonly List<int> _input = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        [Benchmark]
+        public Dictionary<string, List<int>> BenchmarkGroupByEvenOdd()
+        {
+            return GroupAndSortByEvenOdd.Program.GroupSortByEvenOdd(_input);
+        }
+        
+        [Benchmark]
+        public Dictionary<string, List<int>> BenchmarkGroupByEvenOdd2()
+        {
+            return GroupAndSortByEvenOdd.Program.GroupSortByEvenOdd(_input);
+        }
+        
+        [Benchmark]
+        public Dictionary<string, List<int>> BenchmarkGroupByEvenOdd3()
+        {
+            return GroupAndSortByEvenOdd.Program.GroupSortByEvenOdd(_input);
+        }
+    }
+}
---
+++ exercises/c#/GroupAndSortByEvenOdd/GroupAndSortByEvenOdd/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace GroupAndSortByEvenOdd;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> input = new() { 10, 15, 3, 7, 2, 8, 23, 14 };
+        
+        var response = GroupSortByEvenOdd3(input);
+
+        foreach (var key in response.Keys)
+        {
+            Console.Write($"{key}: ");
+            foreach (var number in response[key])
+            {
+                Console.Write($"{number} ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+
+    public static Dictionary<string, List<int>> GroupSortByEvenOdd(List<int> input)
+    {
+        if (input is null) throw new ArgumentNullException("Input cannot be null");
+        
+        input.Sort();
+        
+        Dictionary<string, List<int>> output = new();
+        
+        output.Add("even", new List<int> {});
+        output.Add("odd", new List<int> {});
+
+        foreach (var number in input)
+        {
+            if (number % 2 == 0)
+            {
+                output["even"].Add(number);
+            }
+            else
+            {
+                output["odd"].Add(number);
+            }
+            
+        }
+        
+        return output;
+    }
+
+    public static Dictionary<string, List<int>> GroupSortByEvenOdd2(
+        List<int> input)
+    {
+        if (input is null) throw new ArgumentNullException("Input cannot be null");
+        
+        Dictionary<string, List<int>> output = new();
+        output.Add("even", input.Where(x => x % 2 == 0).OrderBy(x => x).ToList());
+        output.Add("odd", input.Where(x => x % 2 != 0).OrderBy(x => x).ToList());
+        
+        return output;
+    }
+
+    public static Dictionary<string, List<int>> GroupSortByEvenOdd3(List<int> input)
+    {
+        if (input is null) throw new ArgumentNullException("Input cannot be null");
+
+        return input
+            .GroupBy(x => x % 2 == 0 ? "even" : "odd")
+            .ToDictionary(x => x.Key, x => x.OrderBy(y => y).ToList());
+    }
+}
